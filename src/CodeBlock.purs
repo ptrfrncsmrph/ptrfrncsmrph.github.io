0223f23623a36a6ee43bc8e30aedb3a5f3392a20
Peter Murphy <pete@simspace.com>
2020-12-11 23:54:34 -0500

    Add comment to CodeBlock

--- src/CodeBlock.purs
+++ src/CodeBlock.purs
@@ -2,30 +2,36 @@ module CodeBlock where
 
 import Prelude
 import Data.Array ((!!))
+import Data.Nullable (Nullable)
 import Debug.Trace (trace)
 import Effect.Class.Console (log)
 import Effect.Unsafe (unsafePerformEffect)
+import React.Basic.DOM (CSS, css)
 import React.Basic.Hooks (Component, JSX, component)
 import React.Basic.Hooks as React
 import Unsafe.Coerce (unsafeCoerce)
 
+-- | We're currently 'unsafeCoerce'-ing this in the only place where it is
+-- | currently being used, so can probably get rid of the types here (use
+-- | Foreign something or other?)
 mkCodeBlock ::
   Component
     { children :: Array JSX
     , className :: String
-    , extraClassName :: String
+    , extraClassName :: Nullable String
+    , style :: CSS
     }
 mkCodeBlock = do
   codeBlock <- mkCodeBlock'
   component "CodeBlock" \props ->
-    trace (unsafeCoerce props.children !! 4) \_ -> React.do
-      pure (codeBlock props)
+    pure (codeBlock props { style = css { color: "red" } })
 
 mkCodeBlock' ::
   Component
     { children :: Array JSX
     , className :: String
-    , extraClassName :: String
+    , extraClassName :: Nullable String
+    , style :: CSS
     }
 mkCodeBlock' = codeBlock_
 
@@ -33,5 +39,6 @@ foreign import codeBlock_ ::
   Component
     { children :: Array JSX
     , className :: String
-    , extraClassName :: String
+    , extraClassName :: Nullable String
+    , style :: CSS
     }
